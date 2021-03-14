7bf13cd3968e817acaa3a62841917eea5a061a0e
lsminter <lsminteris@gmail.com>
2021-03-14 14:09:51 -0700

    moved the login logic to the login page

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import Head from 'next/head'
 import Footer from '../components/footer'
 import Header from '../components/header'
 
-import { useAuth } from '../lib/auth';
 
 export default function Home() {
-  const auth = useAuth();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-0 px-2">
       <Header />
@@ -18,12 +16,6 @@ export default function Home() {
         <h1 className="text-6xl m-0">
           <a>Welcome to my Gaming Articles Website!!</a>
         </h1>
-        <button onClick={() => auth.signinWithGithub()}>Sign In</button>
-        <div>{auth?.user?.email}</div>
-        {auth?.user && (
-          <button onClick={() => auth.signout()}>Sign Out</button>
-        )}
-
       </main>
 
       <Footer />
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,11 @@
 import Header from '../components/header'
 import Footer from '../components/footer'
 
+import { useAuth } from '../lib/auth';
+
 export default function Login () {
+  const auth = useAuth();
+
   return(
     <div className="flex flex-col items-center justify-center min-h-screen py-0 px-2">
       <Header />
@@ -9,19 +13,11 @@ export default function Login () {
         <h1 className="text-6xl m-0">
           <a>Login Page!!</a>
         </h1>
-        <form>
-          <div className="flex flex-col items-center justify-center flex-grow px-0 py-20">
-            <div className="m-2">
-              <input className="border-black border-2 rounded" type="email" placeholder="Email" name="email" required/>
-            </div>
-            <div className="m-2">
-              <input className="border-black border-2 rounded" type="password" placeholder="Password" name="password" required/>
-            </div>
-            <div className="m-2">
-              <button className="border-black border-2 pl-3 pr-3 rounded hover:bg-gray-400" type="submit">Login</button>
-            </div>
-          </div>
-        </form>
+        <button onClick={() => auth.signinWithGithub()}>Sign In</button>
+        <div>{auth?.user?.email}</div>
+        {auth?.user && (
+          <button onClick={() => auth.signout()}>Sign Out</button>
+        )}
       </main>
         <Footer />
     </div>
