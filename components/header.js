8c9d0aea0dfa2132e412e95c3d2e5432ff588f12
lsminter <lsminteris@gmail.com>
2021-04-14 12:27:11 -0700

    added database file to link data to firestore

--- components/header.js
+++ components/header.js
@@ -8,9 +8,6 @@ export default function Header() {
   return(
     <div className="flex flex-wrap bg-gray-400 w-screen justify-between">
       <Link href="/"><h1 className="text-3xl">This is my header!</h1></Link>
-        {auth?.user && (
-          <div>Logged in as: {auth?.user?.email}</div>
-        )}
       <div className="flex flex-wrap items-center mr-5">
         <Link href="/new">
           <h1 className="ml-2 mr-2 hover:text-white item-start">New Article</h1>
@@ -19,10 +16,17 @@ export default function Header() {
           <h1 className="ml-2 mr-2 hover:text-white item-start">Home</h1>
         </Link>
         {!auth.user && (
-          <button className="ml-2 mr-2 hover:text-white" onClick={() => auth.signinWithGithub()}>Sign In</button>
+          <button className="ml-2 mr-2 hover:text-white" onClick={() => auth.signinWithGitHub()}>Sign In</button>
         )}
         {auth?.user && (
             <button className="ml-2 mr-2 hover:text-white" onClick={() => auth.signout()}>Sign Out</button>
+        )}
+        {auth?.user && (
+          <div>
+            <div className="sm:w-2/12 px-4">
+              <img src={auth?.user?.photoUrl} className="shadow rounded-full max border-none"/>
+            </div>
+          </div>
         )}
       </div>
     </div>
